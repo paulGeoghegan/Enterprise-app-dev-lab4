cf9694b2a6c46f8aab94c6e9d7b39ccb3a14572e
paulGeoghegan <mrpaultdgeoghegan@gmail.com>
2023-05-05 19:15:23 +0000

    Made page numbers more accessible and added update and delete buttons for each product

--- Public/home/productsScript.js
+++ Public/home/productsScript.js
@@ -7,7 +7,6 @@ let fullProductsList = []
 let currentProductsList = [];
 
 let startTime = new Date().getTime();
-
 $.get("/getProducts").done(function(productsList) {generate(productsList)});
 
 //This will generate the page with default options
@@ -18,7 +17,7 @@ function generate(productsList) {
 
 	productsDiv.empty();
 	//Adds heading to table
-	productsTable.append(`<thead><th>Image</th><th>Name</th><th>Price</th><th>Rating</th><th>Stock Level</th><th>Brand</th><th>Category</th></thead>`);
+	productsTable.append(`<thead><th>Image</th><th>Name</th><th>Price</th><th>Rating</th><th>Stock Level</th><th>Brand</th><th>Category</th><th>Actions</th></thead>`);
 
 	//Adds page numbers
 	createPages(productsList);
@@ -49,38 +48,39 @@ function createPages(productsList) {
 	else{numberOfPages=(currentProductsList.length/10)+1+1}
 
 	//Adds previous page arrow
-	productsPagesDiv.append(`
-		<span class="navButtons" onclick="changePageUsingArrows(-1)">&#8592;</span>
-	`);
+	productsPagesDiv.append(`<span class="navButtons" tabindex="0" onclick="changePageUsingArrows(-1)">&#8592;</span>`);
 
 	//Loops to create page numbers
 	for(let i = 1;i<=numberOfPages;i++) {
 		//Adds page Numbers
-		productsPagesDiv.append(`<span id="pageNumber`+i+`" class="pageNumber" onclick="changeCurrentPage(`+i+`)">`+i+`</span>`);
+		productsPagesDiv.append(`<span id="pageNumber`+i+`" class="pageNumber" tabindex="0" onclick="changeCurrentPage(`+i+`)">`+i+`</span>`);
 	}
 
 	//Adds next page navigation arrow
 	productsPagesDiv.append(`
-		<span class="navButtons" onclick="changePageUsingArrows(1)">&#8594;</span>
+		<span class="navButtons" tabindex="0" onclick="changePageUsingArrows(1)">&#8594;</span>
 	`);
 
-	//Goes to the first page
-	changeCurrentPage(1);
+	//Goes to the first page and uses setTimeout to ensure styling is after css has been applied
+setTimeout(function(){changeCurrentPage(1)},100);
 }
 
 //Changes the products to reflect the current page
 function changeCurrentPage(pageNumber) {
 	productsTableBody.empty();
-
 	//Adds products from current page to page
 	for(let i = (pageNumber*10)-10;i<pageNumber*10;i++) {
-		productsTableBody.append(`<tr><td class="thumbnailColumn"><img src="`+currentProductsList[i].thumbnail+`" alt="`+currentProductsList[i].title+` thumbnail" width="100%" ></td><td><a tabindex="0" onclick="getProduct(`+currentProductsList[i].id+`)">`+currentProductsList[i].title+`</a></td><td>RRP: <span class="originalPrice">€`+currentProductsList[i].price+`</span> - `+currentProductsList[i].discountPercentage+`%</br><span class="saleSpan">Sale Price: €`+((currentProductsList[i].price/100)*(100-currentProductsList[i].discountPercentage)).toFixed(2)+`</span></td><td>`+currentProductsList[i].rating+`</td><td>`+currentProductsList[i].stock+`</td><td>`+currentProductsList[i].brand+`</td><td>`+currentProductsList[i].category+`</td></tr>`)
+		productsTableBody.append(`<tr><td class="thumbnailColumn"><img src="`+currentProductsList[i].thumbnail+`" alt="`+currentProductsList[i].title+` thumbnail" width="100%" ></td><td><a tabindex="0" onclick="getProduct(`+currentProductsList[i].id+`)">`+currentProductsList[i].title+`</a></td><td>RRP: <span class="originalPrice">€`+currentProductsList[i].price+`</span> - `+currentProductsList[i].discountPercentage+`%</br><span class="saleSpan">Sale Price: €`+((currentProductsList[i].price/100)*(100-currentProductsList[i].discountPercentage)).toFixed(2)+`</span></td><td>`+currentProductsList[i].rating+`</td><td>`+currentProductsList[i].stock+`</td><td>`+currentProductsList[i].brand+`</td><td>`+currentProductsList[i].category+`</td><td><button class="productActions" onclick="updateProduct(`+currentProductsList[i].id+`)">Update `+currentProductsList[i].title+`</button></br><button class="productActions" onclick="deleteProduct(`+currentProductsList[i].id+`)">Delete `+currentProductsList[i].title+`</button></td></tr>`)
 	}
 
 	//Resets page number styles
 	$(".pageNumber").css({"background-color":"lightgray","color":"black"});
+	//Clears aria-current
+	$(".pageNumber").attr("aria-current",false);
 
 	//Changes highlighted number
 	$("#pageNumber"+pageNumber).css({"background-color":"black","color":"lightgray"});
+	//This lets screen readers know what page is selected
+$("#pageNumber"+pageNumber).attr("aria-current",true);
 
 }
